852ef2e4f5e89db9d29f98ea0bed528a4ad7eb84
goel-anubhav <anubhavgoayl12062001@gmail.com>
2024-06-05 09:52:47 +0000

    Updated

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.getData)
+    path('',views.getData),
+    path('add/', views.addData)
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+from rest_framework.response import Response 
 from rest_framework.decorators import api_view
 from .serializer import ItemSerializer
 from base.models import Item
@@ -8,4 +8,11 @@ from base.models import Item
 def getData(request):
     items=Item.objects.all()
     serializer=ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addData(request):
+    serializer=ItemSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
     return Response(serializer.data)
