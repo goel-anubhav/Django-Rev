3c5268904a44c85cc83066c632827a99220f1bcb
unknown <anubhavgoyal12062001@gmail.com>
2024-06-11 05:42:33 +0000

    CRUD Django Completed

--- api/views.py
+++ api/views.py
@@ -4,15 +4,29 @@ from .serializer import ItemSerializer
 from base.models import Item
 
 
-@api_view(['GET'])
+@api_view(['GET','POST','PUT','DELETE'])
 def getData(request):
-    items=Item.objects.all()
-    serializer=ItemSerializer(items, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+         items=Item.objects.all()
+         serializer=ItemSerializer(items, many=True)
+         return Response(serializer.data)
+         
+    elif request.method=='POST':
+         serializer=ItemSerializer(data = request.data)
+         if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data)
 
-@api_view(['POST'])
-def addData(request):
-    serializer=ItemSerializer(data = request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    elif request.method=='PUT':
+        data = request.data
+        obj = Item.objects.get(id=data['id'])
+        serializer=ItemSerializer(obj, data = data )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+    else:
+            data = request.data
+            obj = Item.objects.get(id=data['id'])
+            obj.delete()
+            return Response({'message':'Item Deleted'})
